edf869121f1a7cd0dcd00519a89ded1a3177cd08
Steve South <stephen.south@caplin.com>
2013-05-19 00:05:37 +1000

    Same as previous..

--- src/com/caplin/filter/filters/WeightByCharacterGrouping.java
+++ src/com/caplin/filter/filters/WeightByCharacterGrouping.java
@@ -6,6 +6,8 @@ import com.caplin.filter.MatchList;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created with IntelliJ IDEA.
@@ -20,7 +22,7 @@ public class WeightByCharacterGrouping implements Filter {
         /*
         Filter chain 2: Weight by grouped characters and sort
          */
-        for (int loop = 2, length = searchFor.length(); loop < length; loop++) {
+        for (int loop = 1, length = searchFor.length(); loop <= length; loop++) {
             ArrayList<String> parts = getParts(searchFor, loop);
 
             for (int i = 0, l = listToSearch.size(); i < l; i = i + 1) {
@@ -28,8 +30,15 @@ public class WeightByCharacterGrouping implements Filter {
                 String matchName = match.getName().toLowerCase();
 
                 for (int lc = 0, pl = parts.size(); lc < pl; lc++)     {
-                    if (matchName.matches(".*" + parts.get(lc) + ".*")) {
-                        match.increaseRating(pl * 1000);
+
+                    Pattern pattern = Pattern.compile(parts.get(lc));
+                    Matcher matcher = pattern.matcher(matchName);
+
+                    int matchCount = 0;
+                    while (matcher.find()) matchCount++;
+
+                    if (matchCount > 0) {
+                        match.increaseRating(loop * 1000 + (matchCount * loop * 100));
                     };
                 }
             }
@@ -40,9 +49,9 @@ public class WeightByCharacterGrouping implements Filter {
 
     private ArrayList<String> getParts(String currentValue, int sectionLength) {
         ArrayList<String> parts = new ArrayList<String>();
-        for (int i = 0, l = currentValue.length(); i + sectionLength < l; i++) {
+        for (int i = 0, l = currentValue.length(); i + sectionLength <= l; i++) {
             try {
-                parts.add(currentValue.substring(i, sectionLength));
+                parts.add(currentValue.substring(i, i + sectionLength));
             } catch (Exception e) {
 
             }
--- tests/tests/com/caplin/filter/filters/WeightByCharacterGroupingTest.java
+++ tests/tests/com/caplin/filter/filters/WeightByCharacterGroupingTest.java
@@ -3,6 +3,7 @@ package tests.com.caplin.filter.filters;
 import com.caplin.filter.Match;
 import com.caplin.filter.MatchList;
 import com.caplin.filter.filters.IncludesAllCharactersInOrder;
+import com.caplin.filter.filters.WeightByCharacterGrouping;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,7 +19,7 @@ import static org.junit.Assert.assertEquals;
 public class WeightByCharacterGroupingTest {
 
     private MatchList before;
-    private IncludesAllCharactersInOrder filter;
+    private WeightByCharacterGrouping filter;
 
     @Before
     public void setUp() {
@@ -26,63 +27,67 @@ public class WeightByCharacterGroupingTest {
         before.add(new Match("axabbccddee", 0));
         before.add(new Match("bbccddeeff", 0));
         before.add(new Match("eeffgghhii", 0));
-        before.add(new Match("ageik", 0));
+        before.add(new Match("ageddkkddkkdd", 0));
 
-        filter = new IncludesAllCharactersInOrder();
+        filter = new WeightByCharacterGrouping();
     }
 
     @Test
-    public void basicFilter() {
+    public void basicGrouping() {
         MatchList after = filter.run("aabb", before);
-        assertEquals(1, after.size());
-        assertEquals("axabbccddee", ((Match)after.get(0)).getName());
-    }
-
-    @Test
-    public void multipleResults() {
-        MatchList after = filter.run("ee", before);
-        assertEquals(3, after.size());
-        assertEquals("axabbccddee",((Match)after.get(0)).getName());
-        assertEquals("bbccddeeff", ((Match)after.get(1)).getName());
-        assertEquals("eeffgghhii", ((Match)after.get(2)).getName());
-    }
-
-    @Test
-    public void characterSeparation() {
-        MatchList after = filter.run("bf", before);
-        assertEquals(1, after.size());
-        assertEquals("bbccddeeff", ((Match)after.get(0)).getName());
-    }
-
-    @Test
-    public void emptySearch() {
-        MatchList after = filter.run("", before);
         assertEquals(4, after.size());
-        assertEquals("axabbccddee",((Match)after.get(0)).getName());
+        assertEquals("axabbccddee", ((Match)after.get(0)).getName());
         assertEquals("bbccddeeff", ((Match)after.get(1)).getName());
-        assertEquals("eeffgghhii", ((Match)after.get(2)).getName());
-        assertEquals("ageik", ((Match)after.get(3)).getName());
+        assertEquals("ageddkkddkkdd", ((Match)after.get(2)).getName());
+        assertEquals("eeffgghhii", ((Match)after.get(3)).getName());
     }
 
     @Test
-    public void ignoresWhitespace() {
-        MatchList after = filter.run("   eef    ", before);
-        assertEquals(2, after.size());
+    public void sameWeightingKeepsOrder() {
+        MatchList after = filter.run("ef", before);
+        assertEquals(4, after.size());
         assertEquals("bbccddeeff", ((Match)after.get(0)).getName());
         assertEquals("eeffgghhii", ((Match)after.get(1)).getName());
+        assertEquals("axabbccddee", ((Match)after.get(2)).getName());
+        assertEquals("ageddkkddkkdd", ((Match)after.get(3)).getName());
     }
 
     @Test
-    public void multipleCharacterSeparation() {
-        MatchList after = filter.run("ae", before);
-        assertEquals(2, after.size());
-        assertEquals("axabbccddee", ((Match)after.get(0)).getName());
-        assertEquals("ageik", ((Match)after.get(1)).getName());
+    public void longerSearch() {
+        MatchList after = filter.run("bbddf", before);
+        assertEquals(4, after.size());
+        assertEquals("bbccddeeff", ((Match)after.get(0)).getName());
+        assertEquals("axabbccddee", ((Match)after.get(1)).getName());
+        assertEquals("ageddkkddkkdd", ((Match)after.get(2)).getName());
+        assertEquals("eeffgghhii", ((Match)after.get(3)).getName());
     }
 
     @Test
-    public void noResults() {
-        MatchList after = filter.run("awlkjlwekfjewe", before);
-        assertEquals(0, after.size());
+     public void anotherSearch() {
+        MatchList after = filter.run("ahii", before);
+        assertEquals(4, after.size());
+        assertEquals("eeffgghhii", ((Match)after.get(0)).getName());
+        assertEquals("axabbccddee", ((Match)after.get(1)).getName());
+        assertEquals("ageddkkddkkdd", ((Match)after.get(2)).getName());
+        assertEquals("bbccddeeff", ((Match)after.get(3)).getName());
     }
+
+    @Test
+    public void moreMatchesWins() {
+        before = new MatchList();
+        before.add(new Match("abskkjkjkjk", 0));
+        before.add(new Match("akskjsdkjaksjksja", 0));
+        before.add(new Match("akjskjsjkjkakjjskjskjakjkjskja", 0));
+        before.add(new Match("aksjskajkakjajajaaaa", 0));
+
+        MatchList after = filter.run("a", before);
+        assertEquals(4, after.size());
+
+        assertEquals("aksjskajkakjajajaaaa", ((Match)after.get(0)).getName());
+        assertEquals("akjskjsjkjkakjjskjskjakjkjskja", ((Match)after.get(1)).getName());
+        assertEquals("akskjsdkjaksjksja", ((Match)after.get(2)).getName());
+        assertEquals("abskkjkjkjk", ((Match)after.get(3)).getName());
+    }
+
+
 }
